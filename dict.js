4077fbfcc917118fd582e6fc890496bd14ea8ebd
kanteshprasad <hk.kanteshprasad@gmail.com>
2024-03-06 22:12:30 +0530

    fixed file path

--- dict.js
+++
@@ -1,9 +0,0 @@
-let obj = {
-  ಆಗಿಸು: "let",
-  ಬರೆ: "console.log",
-  ಇವಾಗ: "if",
-  ಆದರೂ: "else if",
-  ಅಥವ: "else",
-  ಕಾರ್ಯ:"function",
-  
-}
---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+'use client'
+import TextEditor from "./components/Editor";
+
+
+
+
+export default function Home() {
+
+
+  return (
+    <TextEditor></TextEditor>
+    
+  
+  );
+}
